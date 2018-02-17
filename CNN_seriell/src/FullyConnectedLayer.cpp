d1f8a1a428e84a30e628c92c176946350a32c061
Florian Schmidt <it.schmflo@gmail.com>
2018-02-17 20:05:15 +0100

    implemented backpropagation for fully connected layer

--- CNN_seriell/src/FullyConnectedLayer.cpp
+++ CNN_seriell/src/FullyConnectedLayer.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "FullyConnectedLayer.hpp"
+#include "mathematics.h"
 
 FullyConnected_Layer::FullyConnected_Layer(int size) : Layer(size, FULLY_CONNECTED_LAYER) {
 
@@ -24,5 +25,32 @@ void FullyConnected_Layer::backpropagate( Matrix* inputs,
 							Matrix* weight_derivations,
 							Matrix* bias_derivations )
 {
-
+	int outputsize = activations->getHeight();
+	int inputsize = weights->getLength();
+	Matrix *y_deriv_z = new Matrix(outputsize, 1);
+	Matrix *y_deriv_x = new Matrix(outputsize, inputsize);
+	// calc y_deriv_z
+	mathematics::sigmoid_backward_derivated(activations->get(), y_deriv_z->get(), outputsize);
+	// calc input derivations
+	for(int i=0; i<inputsize; i++)
+	{
+		input_derivations->set(i, 0, 0.0);
+		for(int o=0;o<outputsize; o++)
+		{
+			y_deriv_x->set(o,i, y_deriv_z->get(o,0) * weights->get(o,i) );
+			input_derivations->set(i, 0, input_derivations->get(i,0) + activation_derivations->get(i,0) * y_deriv_x->get(o,i) );
+		}
+	}
+	// calc weight- and bias-derivations
+	for(int o=0;o<outputsize; o++)
+	{
+		for(int i=0; i<inputsize; i++)
+		{
+			weight_derivations->set(o,i, activation_derivations->get(o,0) * y_deriv_z->get(o,0) * inputs->get(i,0) );
+		}
+		bias_derivations->set(o,0, activation_derivations->get(o,0) * y_deriv_z->get(o,0) );
+	}
+	// clean up locals
+	delete y_deriv_z;
+	delete y_deriv_x;
 }
